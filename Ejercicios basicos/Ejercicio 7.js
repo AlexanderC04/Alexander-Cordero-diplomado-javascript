ca9e4475370d9be3daf7d4e8b6d6236d799dbe77
Alexanderc04 <thealexandercordero.11@gmail.com>
2022-04-12 22:09:41 -0400

    prompt npm

--- Ejercicios basicos/Ejercicio 7.js
+++ Ejercicios basicos/Ejercicio 7.js
@@ -1,8 +1,10 @@
 //7. Escribe un programa que pida un número y diga si es divisible por 2.
 const ps = require("prompt-sync");
 const prompt = ps();
-let num = parseInt(prompt("Primer Numero?"));
 
+let num = parseInt(prompt("Primer Numero? "));
 if (num % 2 == 0) {
-  console.log("lol");
+  console.log(num + " es divisible por 2");
+} else {
+  console.log(num + " NO es divisible por 2");
 }
